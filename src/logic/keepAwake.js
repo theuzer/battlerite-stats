7481dc4298063d8dfb0ea7ea8562149ab9deaa4f
theuzer <jarvandemacio@gmail.com>
2018-02-20 16:50:33 +0000

    devs

--- src/logic/keepAwake.js
+++ src/logic/keepAwake.js
@@ -2,7 +2,7 @@ const https = require('https');
 
 const keepAwake = () => {
   setInterval(() => {
-    https.get('');
+    https.get('https://battlerite-stats.herokuapp.com/');
     console.log('pinged application');
   }, 300000);
 };
