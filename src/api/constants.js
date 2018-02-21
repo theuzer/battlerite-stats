dd7175360e1f645ee86fa2475b2505aecbc8591b
theuzer <jarvandemacio@gmail.com>
2018-02-21 15:42:10 +0000

    devs

--- src/api/constants.js
+++ src/api/constants.js
@@ -3,6 +3,6 @@ module.exports = {
   statsBase: 'stats?',
   timePeriodFilter: (filter) => filter === null ? '' : `timePeriod=${filter}&`,
   rankedFilter: (filter) => filter === null ? '' : `ranked=${filter}&`,
-  teamSizeFilter: (filter) => filter === null ? '' : `teamSize=${filter}&`,
+  teamSizeFilter: (filter) => filter === null ? '' : `mode=${filter}&`,
   leagueFilter: (filter) => filter === null ? '' : `league=${filter}&`,
 };
