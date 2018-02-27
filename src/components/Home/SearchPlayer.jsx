48ef3396e323cab261888a03a5b667016d971f4c
theuzer <jarvandemacio@gmail.com>
2018-02-27 15:27:20 +0000

    devs

--- src/components/Home/SearchPlayer.jsx
+++ src/components/Home/SearchPlayer.jsx
@@ -7,6 +7,7 @@ class SearchPlayer extends Component {
       playerName: '',
     };
     this.handleOnPlayerNameChange = this.handleOnPlayerNameChange.bind(this);
+    this.handleOnEnterKeyPress = this.handleOnEnterKeyPress.bind(this);
   }
 
   handleOnPlayerNameChange(e) {
@@ -15,10 +16,21 @@ class SearchPlayer extends Component {
     });
   }
 
+  handleOnEnterKeyPress() {
+    if (this.state.playerName !== '') {
+      this.props.history.push(`/${this.state.playerName}`);
+    }
+  }
+
   render() {
     return (
       <div>
-        <input placeholder="Search player" value={this.state.playerName} />
+        <input
+          placeholder="Search player"
+          value={this.state.playerName}
+          onChange={this.handleOnPlayerNameChange}
+          onKeyPress={this.handleOnEnterKeyPress}
+        />
       </div>
     );
   }
