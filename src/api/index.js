dc37f2836abc1ebb47c5d391e2f6619828b44bd5
theuzer <jarvandemacio@gmail.com>
2018-03-02 10:02:20 +0000

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@ const axios = require('axios');
 const constants = require('./constants');
 const filters = require('../static/filters/index');
 
+//
+// Homescreen Stats
+//
 const getStatsFilterValue = (chosenFilters, filterId) => {
   if (chosenFilters === null || typeof (chosenFilters) === 'undefined') {
     return null;
@@ -19,20 +22,37 @@ const getStatsUrl = (chosenFilters) => {
   const teamSizeFilter = getStatsFilterValue(chosenFilters, 2);
   const leagueFilter = getStatsFilterValue(chosenFilters, 3);
 
-  return constants.host + constants.stats.base + constants.stats.timePeriodFilter(timePeriodFilter) +
-         constants.stats.rankedFilter(rankedFilter) + constants.stats.teamSizeFilter(teamSizeFilter) +
+  return constants.host + constants.stats.base +
+         constants.stats.timePeriodFilter(timePeriodFilter) +
+         constants.stats.rankedFilter(rankedFilter) +
+         constants.stats.teamSizeFilter(teamSizeFilter) +
          constants.stats.leagueFilter(leagueFilter);
 };
 
-exports.getStatsUrl = (chosenFilters) => {
+const getStats = (chosenFilters) => {
   const url = getStatsUrl(chosenFilters);
   console.log(url);
+
   return axios.get(url);
 };
 
-exports.getPlayerMatches = (playerName, page) => {
-  const url = constants.host + constants.playerMatches.base +
-              constants.playerMatches.playerNameFilter(playerName) +
-              constants.playerMatches.pageFilter(page);
+//
+// Player matches
+//
+
+const getPlayerMatchesUrl = (playerName, page) =>
+  constants.host + constants.playerMatches.base +
+  constants.playerMatches.playerNameFilter(playerName) +
+  constants.playerMatches.pageFilter(page);
+
+const getPlayerMatches = (playerName, page) => {
+  const url = getPlayerMatchesUrl(playerName, page);
+  console.log(url);
+
   return axios.get(url);
+};
+
+module.exports = {
+  getStats,
+  getPlayerMatches,
 };
