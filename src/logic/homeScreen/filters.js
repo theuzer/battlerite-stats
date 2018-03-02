a2a9f3809d9cfeeb0d00f91cbc1c3b2a670825be
theuzer <jarvandemacio@gmail.com>
2018-03-02 14:13:13 +0000

    refactoring

--- src/logic/homeScreen/filters.js
+++ src/logic/homeScreen/filters.js
@@ -1,27 +1,44 @@
 module.exports = [
   {
     id: 0,
-    label: 'Total games',
-    prop: 'totalGames',
+    label: 'Time period',
+    options: [
+      { key: 0, value: 'All time', api: '0' },
+      { key: 1, value: 'Last month', api: '1' },
+      { key: 2, value: 'Last week', api: '2' },
+      { key: 3, value: 'Yesterday', api: '3' },
+    ],
   },
   {
     id: 1,
-    label: 'Win rate',
-    prop: 'winRate',
+    label: 'Queue',
+    options: [
+      { key: 0, value: 'All queues', api: null },
+      { key: 1, value: 'Ranked', api: true },
+      { key: 2, value: 'Casual', api: false },
+    ],
   },
   {
     id: 2,
-    label: 'Avg damage',
-    prop: 'avgDamage',
+    label: 'Mode',
+    options: [
+      { key: 0, value: 'All modes', api: null },
+      { key: 1, value: '2v2', api: '2' },
+      { key: 2, value: '3v3', api: '3' },
+    ],
   },
   {
     id: 3,
-    label: 'Avg healing',
-    prop: 'avgHeal',
-  },
-  {
-    id: 4,
-    label: 'Avg disables',
-    prop: 'avgDisables',
+    label: 'League',
+    options: [
+      { key: 0, value: 'All leagues', api: null },
+      { key: 1, value: 'Bronze', api: '0' },
+      { key: 2, value: 'Silver', api: '1' },
+      { key: 3, value: 'Gold', api: '2' },
+      { key: 4, value: 'Platinum', api: '3' },
+      { key: 5, value: 'Diamond', api: '4' },
+      { key: 6, value: 'Champion', api: '5' },
+      { key: 7, value: 'Grand Champion', api: '6' },
+    ],
   },
 ];
--- src/logic/homeScreen/sorts.js
+++ src/logic/homeScreen/sorts.js
@@ -1,44 +1,27 @@
 module.exports = [
   {
     id: 0,
-    label: 'Time period',
-    options: [
-      { key: 0, value: 'All time', api: '0' },
-      { key: 1, value: 'Last month', api: '1' },
-      { key: 2, value: 'Last week', api: '2' },
-      { key: 3, value: 'Yesterday', api: '3' },
-    ],
+    label: 'Total games',
+    prop: 'totalGames',
   },
   {
     id: 1,
-    label: 'Queue',
-    options: [
-      { key: 0, value: 'All queues', api: null },
-      { key: 1, value: 'Ranked', api: true },
-      { key: 2, value: 'Casual', api: false },
-    ],
+    label: 'Win rate',
+    prop: 'winRate',
   },
   {
     id: 2,
-    label: 'Mode',
-    options: [
-      { key: 0, value: 'All modes', api: null },
-      { key: 1, value: '2v2', api: '2' },
-      { key: 2, value: '3v3', api: '3' },
-    ],
+    label: 'Avg damage',
+    prop: 'avgDamage',
   },
   {
     id: 3,
-    label: 'League',
-    options: [
-      { key: 0, value: 'All leagues', api: null },
-      { key: 1, value: 'Bronze', api: '0' },
-      { key: 2, value: 'Silver', api: '1' },
-      { key: 3, value: 'Gold', api: '2' },
-      { key: 4, value: 'Platinum', api: '3' },
-      { key: 5, value: 'Diamond', api: '4' },
-      { key: 6, value: 'Champion', api: '5' },
-      { key: 7, value: 'Grand Champion', api: '6' },
-    ],
+    label: 'Avg healing',
+    prop: 'avgHeal',
+  },
+  {
+    id: 4,
+    label: 'Avg disables',
+    prop: 'avgDisables',
   },
 ];
